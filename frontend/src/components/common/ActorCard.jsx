e4729ee80f74fe0c240ee1a74b36e16cb2ea7c4f
Yevhenyi2024 <karimovzena457@gmail.com>
2025-05-11 15:57:29 +0300

    FIX

--- frontend/src/components/common/ActorCard.jsx
+++ frontend/src/components/common/ActorCard.jsx
@@ -1,17 +1,17 @@
 function ActorCard({ actor }) {
-  const { name, character, photo } = actor;
+  const { name, characters, photo } = actor;
 
   return (
     <div className="bg-white rounded-lg shadow-sm overflow-hidden transition-transform hover:scale-[1.02]">
       <img
         src={photo}
         alt={`Фото актора ${name}`}
-        className="w-full h-40 object-contain" // Змінено на object-contain
+        className="w-full h-40 object-contain" 
       />
 
       <div className="p-3">
         <h3 className="font-semibold text-lg line-clamp-1">{name}</h3>
-        <p className="text-gray-600 text-sm line-clamp-2">{character}</p>
+        <p className="text-gray-600 text-sm line-clamp-2">{characters}</p>
       </div>
     </div>
   );
